0e98dc94e48eed72910b067ea4eb3bb2a30ac4c9
aryakpt <aryakrisna07@gmail.com>
2022-11-03 22:51:17 +0700

    create a custom styles for MUI

---
+++ src/styles/colorPallete.js
@@ -0,0 +1,16 @@
+const colorPallete = {
+  'primary-color': '#16ABF8',
+  'neutral-black': '#111111',
+  'neutral-white': '#fff',
+  'grey-1': '#555555',
+  'grey-2': '#888888',
+  'grey-3': '#E5E5E5',
+  'grey-4': '#F4F4F4',
+  'red-1': '#ED4C5C',
+  'yellow-1': '#FFCE31',
+  'green-1': '#00A790',
+  'blue-1': '#43C4E3',
+  'purple-1': '#B01AFF',
+};
+
+export default colorPallete;
---
+++ src/styles/customThemeMUI.js
@@ -0,0 +1,44 @@
+import { createTheme } from '@mui/material/styles';
+import colorPallete from './colorPallete';
+
+export const customThemeMUI = createTheme({
+  typography: {
+    fontFamily: 'Poppins ,sans-serif',
+    fontSize: 16,
+    htmlFontSize: 16,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+    h1: {
+      fontSize: '36px',
+      lineHeight: '54px',
+      fontWeight: 400,
+    },
+    h2: {
+      fontSize: '24px',
+      lineHeight: '36px',
+      fontWeight: 400,
+    },
+    button: {
+      fontSize: '18px',
+      lineHeight: '27px',
+      fontWeight: 400,
+    },
+  },
+  palette: {
+    custom: {
+      'primary-color': colorPallete['primary-color'],
+      'neutral-black': colorPallete['neutral-black'],
+      'neutral-white': colorPallete['neutral-white'],
+      'grey-1': colorPallete['grey-1'],
+      'grey-2': colorPallete['grey-2'],
+      'grey-3': colorPallete['grey-3'],
+      'grey-4': colorPallete['grey-4'],
+      'red-1': colorPallete['red-1'],
+      'yellow-1': colorPallete['yellow-1'],
+      'green-1': colorPallete['green-1'],
+      'blue-1': colorPallete['blue-1'],
+      'purple-1': colorPallete['purple-1'],
+    },
+  },
+});
