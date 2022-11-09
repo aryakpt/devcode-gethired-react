91993f06a885e783748c81cffd65d83b0a021ce5
aryakpt <aryakrisna07@gmail.com>
2022-11-09 13:37:22 +0700

    23: sort-parent

--- src/components/detail/ModalSort/ModalSort.jsx
+++ src/components/detail/ModalSort/ModalSort.jsx
@@ -14,7 +14,7 @@ const ModalSort = ({ onClose }) => {
   return (
     <>
       <Box sx={styles.backdropModalSort} onClick={onClose}></Box>
-      <Box sx={styles.modalSort}>
+      <Box data-cy="sort-parent" sx={styles.modalSort}>
         <MenuItem
           data-cy="sort-selection"
           sx={styles.modalSortMenuItem}
